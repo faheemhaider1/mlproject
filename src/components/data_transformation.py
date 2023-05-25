4df713b8bfce274f1fd0fbb20c6c4c756f0bacbe
Faheem <faheemfaizan55@yahoo.com>
2023-05-25 18:47:18 +0500

    Data Transformation push-1

---
+++ src/components/data_transformation.py
@@ -0,0 +1,23 @@
+import sys
+import os
+from dataclasses import dataclass
+
+import numpy as np
+import pandas as pd
+from sklearn.compose import ColumnTransformer
+from sklearn.impute import SimpleImputer
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import OneHotEncoder, StandardScaler
+
+from src.exception import Exception
+from src.logger import logging
+
+@dataclass
+class DataTransformationConfig:
+    preprocessor_obj_file_path = os.path.join('artifacts', 'preprocessor.pkl')
+
+class DataTransformation:
+    def __init__(self):
+        self.data_transformation_config = DataTransformationConfig()
+
+    
